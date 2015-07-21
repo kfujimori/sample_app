5af2933a1cab14d57cf34d5ff832c604f2562811
K.Fujimori <atiger7391@gmail.com>
2015-07-21 08:17:55 +0000

    finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,29 +1,46 @@
 require 'spec_helper'
 
 describe "StaticPages" do
-  # describe "GET /static_pages" do
-    # it "works! (now write some real specs)" do
-    #   # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-    #   get static_pages_index_path
-    #   response.status.should be(200)
-    # end
-  # end
+
+
+    # test for Home
     describe "Home page" do
         it "should have the content 'Sample App'" do
             visit '/static_pages/home'
             expect(page).to have_content('Sample App')
         end
+
+        it "should have the right title" do
+            visit '/static_pages/home'
+            expect(page).to have_title('Ruby on Rails Tutorial Sample App | Home')
+        end
     end
+
+
+    # test for help
     describe "Help page" do
         it "should have the content 'Help'" do
             visit '/static_pages/help'
             expect(page).to have_content('Help')
         end
+
+        it "should have the right title" do
+            visit '/static_pages/help'
+            expect(page).to have_title('Ruby on Rails Tutorial Sample App | Help')
+        end
     end
+
+
+    # test for about
     describe "About page" do
         it "should have content 'About Us'" do
             visit '/static_pages/about'
             expect(page).to have_content('About Us')
+        end
+
+        it "should have the right title" do
+            visit '/static_pages/about'
+            expect(page).to have_title('Ruby on Rails Tutorial Sample App | About Us')
         end
     end
 end
