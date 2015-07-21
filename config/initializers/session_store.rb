110c985c74badc618a9d96a850f7d0282bfcb24d
K.Fujimori <atiger7391@gmail.com>
2015-07-21 06:54:47 +0000

    set secret token. install rspec

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,22 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_sample_app_session'
+# Rails.application.config.session_store :cookie_store, key: '_sample_app_session'
+
+
+require "securerandom"
+
+def secure_token
+    token_file = Rails.root.join('.secret')
+    if File.exist?(token_file)
+        # use the existing token
+        File.read(token_file).chomp
+    else
+        # generate a new token and store it in token_file
+        token = SecureRandom.hex(64)
+        File.write(token_file, token)
+        token
+    end
+end
+
+SampleApp::Application.config.secret_key_base = secure_token
+
