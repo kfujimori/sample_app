a6c365239562c61f60bad5adbccc1e6ce2b2b711
K.Fujimori <atiger7391@gmail.com>
2015-07-24 07:03:12 +0000

    add users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
 
+  get 'users/new'
+
   # get 'static_pages/home'
   # get 'static_pages/help'
   # get 'static_pages/about'
@@ -10,6 +12,8 @@ Rails.application.routes.draw do
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
+  match '/signup',  to: 'users#signup',         via: 'get'
+  match '/new',     to: 'users#new',            via: 'get'
 
 
 
---
+++ spec/requests/user_pages_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+describe "UserPages" do
+
+    subject { page }
+
+    describe "signup page" do
+        before { visit signup_path }
+
+        it {should have_content     ('Sign up')}
+        it {should have_title       (full_title('Sign up'))}
+    end
+
+    describe "signup page" do
+        before { visit new_path }
+
+        it {should have_content     ('New')}
+        it {should have_title       (full_title('New'))}
+    end
+
+end
