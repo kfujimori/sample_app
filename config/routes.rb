d9424b32b8af68644102b1d47a2a02c703985087
K.Fujimori <atiger7391@gmail.com>
2015-07-27 08:53:46 +0000

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -8,12 +8,13 @@ Rails.application.routes.draw do
   # get 'static_pages/about'
   # get 'static_pages/contact'
 
+  resources :users
   root  'static_pages#home'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
-  match '/signup',  to: 'users#signup',         via: 'get'
-  match '/new',     to: 'users#new',            via: 'get'
+  # match '/signup',  to: 'users#signup',         via: 'get'
+  match '/signup',     to: 'users#new',            via: 'get'
 
 
 
