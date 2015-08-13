dbd85fbdfe266af4332e6454b3f473dac7b635cf
K.Fujimori <atiger7391@gmail.com>
2015-08-13 06:53:53 +0000

    before updating rspec-rails from 2.13.1 to 2.14.0.rc1

--- Gemfile
+++ Gemfile
@@ -11,6 +11,8 @@ gem 'bootstrap-sass', '2.3.2.0'
 gem 'sprockets', '3.2.0'
 gem 'bcrypt-ruby', '3.1.2'
 
+gem "heroku-api"
+
 group :development, :test do
   gem 'sqlite3', '1.3.8'
   gem 'rspec-rails', '2.13.1'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+
+    before_action :signed_in_user, only: [:edit, :update]
+    before_action :correct_user,   only: [:edit, :update]
+
+
     def index
         @users = User.all
     end
@@ -9,7 +14,7 @@ class UsersController < ApplicationController
         @user = User.new
     end
     def create
-        @user = User.new(user_param)
+        @user = User.new(user_params)
         if @user.save
             # sign up --> sign in
             sign_in @user
@@ -22,14 +27,39 @@ class UsersController < ApplicationController
     # def signup
     # end
 
+    def edit
+    end
+
+    def update
+        if @user.update_attributes(user_params)
+            flash[:success] = "profile Updated!"
+            redirect_to @user
+        else
+            render 'edit'
+        end
+    end
+
+
+
     private
 
-    def user_param
+    def user_params
         params.require(:user).permit(:name,
                                         :email,
                                         :password,
                                         :password_confirmation
         )
+    end
+
+    # before actions
+
+    def signed_in_user
+        redirect_to signin_url, notice: "Please sign in." unless signed_in?
+    end
+
+    def correct_user
+        @user = User.find(params[:id])
+        redirect_to(root_path) unless current_user?(@user)
     end
 
 
